61016b7d13f4d78dcacb7b59f7d727fafae129cd
kami <kami@DESKTOP-1EOEL72>
2025-02-26 19:07:34 +0900

    map 2개로 변경

--- HelloCoherence/src/main/java/com/oracle/coherence/HelloCoherence/HelloCoherence.java
+++ HelloCoherence/src/main/java/com/oracle/coherence/HelloCoherence/HelloCoherence.java
@@ -11,18 +11,34 @@ public class HelloCoherence {
         // System.out.println("Hello World!");
     	
     	{
-            NamedMap<String, String> map = CacheFactory.getCache("welcomes");
+            NamedMap<String, String> map_1 = CacheFactory.getCache("welcomes");
+            
+            NamedMap<String, String> map_2 = CacheFactory.getCache("goodbye");
 
             System.out.printf("Accessing map \"%s\" containing %d entries\n",
-                    map.getName(),
-                    map.size());
+                    map_1.getName(),
+                    map_1.size());
+            
+            System.out.printf("Accessing map \"%s\" containing %d entries\n",
+                    map_2.getName(),
+                    map_2.size());
 
-            map.put("english", "Hello");
-            map.put("spanish", "Hola");
-            map.put("french" , "Bonjour");
+            map_1.put("english", "Hello");
+            map_1.put("spanish", "Hola");
+            map_1.put("french" , "Bonjour");
+            
+            map_2.put("english", "ByeBye");
+            map_2.put("spanish", "Adios");
+            map_2.put("japan" , "Sayonara");
 
             // list
-            map.entrySet().forEach(System.out::println);
+            System.out.println("MapName:"+map_1.getName());
+            map_1.entrySet().forEach(System.out::println);
+            
+            // list
+            System.out.println("MapName:"+map_2.getName());
+            map_2.entrySet().forEach(System.out::println);
+            
             }
     }
 }
